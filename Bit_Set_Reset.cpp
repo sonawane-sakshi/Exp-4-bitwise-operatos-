608217c2d46c203f18f65415dbdaf84212c57db5
sonawane-sakshi <h116sonawanesakshi@gmail.com>
2024-10-04 15:33:15 +0530

    Add files via upload

---
+++ Bit_Set_Reset.cpp
@@ -0,0 +1,20 @@
+//Aditya Sthawarmath
+//PRN: 23070123008
+//Experiment - 4(2)
+//Code to set and reset a bit in a binary number
+
+#include <iostream>
+using namespace std;
+int main(){
+    int i=70,bit_to_be_set,bit_to_be_reset,set,reset;
+    cout<<"Enter position of bit to be set: ";
+    cin>>bit_to_be_set;
+    cout<<"enter position of bit to be reset: ";
+    cin>>bit_to_be_reset;
+    //setting the bit
+    set = i|(1<<bit_to_be_set);
+    //resetting the bit
+    reset = i&(~(1<<bit_to_be_reset));
+    cout<<"set number: "<<set<<endl;
+    cout<<"reset number: "<<reset<<endl;
+}
